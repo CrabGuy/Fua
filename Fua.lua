3642ed8bf396801a4ce5f7ada7394629ee4775a2
Bruno <bruno.direnzo2@gmail.com>
2025-06-23 16:48:26 +0200

    Updated the code, almost the same functionality but cleaner and not as god awful unreadable as before

--- Fua.lua
+++ Fua.lua
@@ -1,169 +1,159 @@
--- I am using lua 5.1 and this is one of the differences i found with the newest version and i want to ensure compatibility
-table.unpack = table.unpack or unpack
+local fua = {}
 
-local function loop(f, i, max, increment)
-    increment = increment or 1
+fua.fill = table.create
+fua.clone = table.clone
 
-    local result = f(i)
-    if i >= max or result ~= nil then
-        return result
-    end
-    return loop(f, i + increment, max, increment)
-end
-
-local function return_after_condition_met(has_to_return)
-    return function (condition)
-        return function (condition_met, return_value)
-            return function (tab, ...)
-                local return_tab = {}
-                local other_args = {...}
-                local loop_result = loop(function (i)
-                    local v = tab[i]
-                    if condition(v, i, other_args, tab) then
-                        local success_result = condition_met(v, i, other_args, tab, return_tab)
-                        if has_to_return then
-                            return success_result
-                        end
-                    end
-                end, 1, #tab)
-                return loop_result or return_value(tab, return_tab)
-            end
-        end
-    end
-end
-
-local function return_true()
-    return true
-end
-local function return_false()
-    return false
-end
-
--- only use this as "return_value" because of the different arguments
-local function return_final_tab(_, return_tab)
-    return return_tab
-end
-
-local empty_func = return_false
-
-table.every = return_after_condition_met(true)(function (v, i, function_arguments)
-    local f = function_arguments[1]
-    return not f(v, i)
-end)(return_false, return_true)
-
-table.filter = return_after_condition_met(false)(function (v, i, function_arguments)
-    local f = function_arguments[1]
-    return f(v, i)
-end)(function (v, _, _, _, return_tab)
-    table.insert(return_tab, v)
-end, return_final_tab)
-
-table.map = return_after_condition_met(false)(return_true)(function (v, i, function_arguments, _, return_tab)
-    local f = function_arguments[1]
-    table.insert(return_tab, f(v, i))
-end, return_final_tab)
-
-table.find = return_after_condition_met(true)(function (v, i, function_arguments)
-    local f = function_arguments[1]
-    return f(v, i)
-end)(function (v, i)
-    return v, i
-end, return_false)
-
-table.flat = function (tab, depth)
-    depth = depth or math.huge
-    local return_tab = {};
-    local function nested(interior_tab, interior_depth)
-        for _, v in pairs(interior_tab) do
-            if type(v) == "table" and interior_depth > 0 then
-                nested(v, interior_depth - 1)
-            else
-                table.insert(return_tab, v)
-            end
-        end
-    end
-    nested(tab, depth)
-    return return_tab
-end
-
-table.includes = return_after_condition_met(true)(function (v, _, function_arguments)
-    local element = function_arguments[1]
-    return element == v
-end)(return_true, return_false)
-
-table.indexOf = return_after_condition_met(true)(function (v, _, function_arguments)
-    local element = function_arguments[1]
-    return element == v
-end)(function (_, i)
-    return i
-end, function ()
-    return -1
-end)
-
-table.flatMap = function (tab, f)
-    return table.flat(table.map(tab, f), 1)
-end
-
-table.reduce = return_after_condition_met(false)(return_true)(function (v, i, function_arguments, tab, accumulator)
-    local f = function_arguments[1]
-    local accumulator_value = accumulator[1] or function_arguments[2] or tab[1]
-    accumulator[1] = f(accumulator_value, v, i)
-end, function (_, accumulator)
-    return accumulator[1]
-end)
-
-table.some = return_after_condition_met(true)(function (v, i, function_arguments)
-    local f = function_arguments[1]
-    return f(v, i)
-end)(return_true, return_false)
-
-table.foreach = return_after_condition_met(false)(return_true)(function (v, i, function_arguments)
-    local f = function_arguments[1]
-    f(v, i)
-end, empty_func)
-
-local function are_equal(a, b)
-    if type(a) ~= type(b) or type(a) ~= "table" then
-        return a == b
-    end
-    if #a ~= #b then
+function fua.is_equal(x, y, prev_x_i, prev_y_i)
+    local different_types = type(x) ~= type(y)
+    if different_types then
         return false
     end
-    for i, v in pairs(a) do
-        if type(v) == "table" then
-            if not are_equal(v, b[i]) then
-                return false
-            end
-        else
-            if v ~= b[i] then
-                return false
+
+    if type(x) ~= "table" then
+        return x == y
+    end
+
+    local x_i, x_v = next(x, prev_x_i)
+    local y_i, y_v = next(y, prev_y_i)
+
+    local is_empty = not x_i and not y_i
+    if is_empty then
+        return true
+    end
+
+    return fua.is_equal(x_v, y_v) and fua.is_equal(x, y, x_i, y_i)
+end
+
+function fua.for_each(f, array, previous)
+    local i, v = next(array, previous)
+
+    if not i then
+        return
+    end
+
+    f(v, i, array)
+
+    return fua.for_each(f, array, i)
+end
+
+function fua.curry(arguments_amount, f, arguments)
+    arguments = arguments or {}
+    return function(arg)
+        table.insert(arguments, arg)
+        if arguments_amount <= 1 then
+            return f(unpack(arguments))
+        end
+        return fua.curry(arguments_amount - 1, f, arguments)
+    end
+end
+
+function fua.reduce(f, accumulator, array, previous)
+    local i, v = next(array, previous)
+    if not i then
+        return accumulator
+    end
+
+    return fua.reduce(f, f(accumulator, v, i, array), array, i)
+end
+
+function fua.map(f, array)
+    local function map(result, v, i, array)
+        result[i] = f(v, i, array)
+        return result
+    end
+
+    return fua.reduce(map, {}, array)
+end
+
+function fua.filter(p, array)
+    local function filter(result, v, i, array)
+        if p(v, i, array) then
+            if #array == 0 then
+                result[i] = v
+            else
+                table.insert(result, v)
             end
         end
+        return result
     end
-    return true
+
+    return fua.reduce(filter, {}, array)
 end
 
-local function toString(x)
-    local return_self = {"number", "string", "boolean", "function"}
-    if type(x) == "nil" then return nil end
-    if table.includes(return_self, type(x)) then return tostring(x) end
-
-    local result = "{"
-    for i, v in pairs(x) do
-        result = result .. "  " .. toString(v)
+function fua.pipe(start, ...)
+    local fs = {...}
+    if #fs == 0 then
+        return start
     end
-    return result.."  }"
+
+    fs[1] = fs[1](start)
+
+    return fua.pipe(unpack(fs))
 end
 
-local function test(f, args, expected_resut)
-    local result = f(table.unpack(args))
-    print("Expected result " .. toString(expected_resut), "got " .. toString(result))
-    if not are_equal(result, expected_resut) then
-        print("ERROR")
+function fua.every(p, array)
+    local function satisfies(result, v, i, array)
+        return result and p(v, i, array)
     end
+    return fua.reduce(satisfies, true, array)
 end
 
+function fua.find(p, array)
+    local function satisfies(result, v, i, array)
+        if result ~= nil then
+            return result
+        elseif p(v, i, array) then
+            return {v, i}
+        else
+            return nil
+        end
+    end
 
+    return unpack(fua.reduce(satisfies, nil, array) or {})
+end
 
+function fua.insert_all(values, array, prev_i)
+    local i, v = next(values, prev_i)
+    if not i then
+        return array
+    end
+    table.insert(array, v)
+    return fua.insert_all(values, array, i)
+end
 
-return table
+function fua.flat(depth, array)
+    local function flatten(result, v)
+        if (type(v) ~= "table") or (depth == 0) then
+            table.insert(result, v)
+            return result
+        else
+            return fua.insert_all(fua.flat(depth - 1, v), result)
+        end
+    end
+
+    return fua.reduce(flatten, {}, array)
+end
+
+function fua.includes(array, target)
+    local function equal(value)
+        return target == value
+    end
+    return fua.find(equal, array) ~= nil
+end
+
+function fua.flat_map(f, array)
+    return fua.pipe(
+        array,
+        fua.curry(2, fua.map)(f),
+        fua.curry(2, fua.flat)(1)
+    )
+end
+
+function fua.some(p, array)
+    local function satisfies(result, v, i, array)
+        return result or p(v, i, array)
+    end
+    return fua.reduce(satisfies, false, array)
+end
+
+return fua
